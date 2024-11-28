bd0944fadc514e65963536f6a7e77b9ded6a9c18
M Asif <148150937+asifKhan014@users.noreply.github.com>
2024-11-28 19:42:22 +0500

    contact form remove from home page

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,7 +1,6 @@
 // import Banner from "@/components/layouts/banner/banner";
 import TourGuide from "../components/layouts/tourGuide/TourGuide";
 import Banner from "../components/layouts/banner/Banner";
-import ContactUs from "../components/forms/contact-us";
 export default function Home() {
   return (
     <div className="">
@@ -11,7 +10,6 @@ export default function Home() {
       {/* 
       <LocalHomeRoom />
       <HotelRoom /> */}
-      <ContactUs />
     </div>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     /* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
---
+++ app/contact-us/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContactUs from "../components/forms/contact-us";
+function page() {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default page;
